581e66fde9c4d88a00e97fcb96b3769157192cb2
Sean Bailey <sean@spb.li>
2020-05-17 00:19:38 -0400

    a bunch of setup

--- src/app/hello.cljs
+++ src/app/hello.cljs
@@ -1,5 +1,6 @@
 (ns app.hello
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [reagent-forms.core :refer [bind-fields]]))
 
 (defn click-counter [click-count]
   [:div
@@ -10,8 +11,37 @@
 
 (def click-count (r/atom 0))
 
+(def login-clicked (r/atom false))
+(def host-clicked (r/atom false))
+
+(def is-host (r/atom false))
+(def user (r/atom {:name ""
+                   :game ""
+                   :pass ""}))
+
+(defn login-buttons []
+  [:div.my-2 
+    [:button.py-2.px-4.rounded.bg-white.font-bold 
+      {:on-click #(reset! login-clicked true)}
+      "Join Game"]
+    [:button.py-2.px-4.rounded.bg-white.font-bold 
+      {:on-click #(reset! host-clicked true)}
+      "Host Game"]])
+
+(defn login-form [is-host] 
+  [:div
+    [:h2 (if is-host "Host Game" "Join Game")]
+    [:input {:type "text" :value "Username"
+             :on-change}]
+    [:input {:type "text" :value "Game Name"}]
+    [:input {:type (if is-host "text" "password") :value "Passkey"}]]
+    [:button {:onclick }])
+
 (defn hello []
   [:<>
-   [:p "Hello, app is running!"]
-   [:p "Here's an example of using a component with state:"]
-   [click-counter click-count]])
+   [:h1 "Scalaria"]
+   [:p "This is the temporary, ugly UI for testing the backend logic and connectivity."]
+   (cond
+    @host-clicked  [:div "Host A Game"]
+    @login-clicked [:div "Log In"]
+    :else           (login-buttons))])
